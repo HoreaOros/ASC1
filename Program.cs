fae536dbf91b4774ba3857b4447102f97174a6ee
Horea Oros <horea.oros@gmail.com>
2019-10-15 13:55:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f = 0.0;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                f += 0.001;
+            }
+
+            Console.WriteLine(f);
+        }
+    }
+}
