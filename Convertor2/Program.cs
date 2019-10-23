06459a1b4525563134f4db90e9a81e2b95d4b291
Horea Oros <horea.oros@gmail.com>
2019-10-23 13:25:49 +0300

    add convertor2

---
+++ Convertor2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 458, baza = 16;
+
+            Console.WriteLine(Convert_10_b(n, baza));
+        }
+
+        private static string Convert_10_b(int n, int baza)
+        {
+            int rest;
+            Stack<int> stiva = new Stack<int>();
+            while (n > 0)
+            {
+                rest = n % baza;
+                stiva.Push(rest);
+                n = n / baza;
+            }
+            StringBuilder sb = new StringBuilder();
+            int cifra;
+            while (stiva.Count > 0)
+            {
+                cifra = stiva.Pop();
+                sb.Append(Num2Digit(cifra));
+            }
+            return sb.ToString();
+        }
+
+        private static char Num2Digit(int cifra)
+        {
+            char ch;
+            if (cifra >= 0 && cifra <= 9)
+                ch = (char)('0' + cifra);
+            else if (cifra >= 10 && cifra <= 15)
+                ch = (char)('A' + cifra - 10);
+            else ch = 'X';
+
+            return ch;
+        }
+    }
+}
