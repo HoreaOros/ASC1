d14d90d83e70dae56ae7da4cd9658bdef3037d55
Horea Oros <horea.oros@gmail.com>
2019-10-24 09:29:32 +0300

    change floating point example

--- Program.cs
+++ Program.cs
@@ -10,17 +10,29 @@ namespace ASC1
     {
         static void Main(string[] args)
         {
-            float f = 0.0f;
+            //float f = 0.0f;
 
-            for (int i = 0; i < 1000000; i++)
-            {
-                f += 0.001f;
-                Console.WriteLine(f);
-                if (i % 25 == 0)
-                    Console.ReadKey();
-            }
+            //for (int i = 0; i < 1000000; i++)
+            //{
+            //    f = f + 0.001f;
+            //    Console.WriteLine(f);
+            //    if (i % 25 == 0) 
+            //        Console.ReadKey();
+            //}
 
-            Console.WriteLine(f);
+            //Console.WriteLine(f);
+
+
+            float a;
+
+            a = 0.001f + 0.001f + 0.001f + 0.001f + 0.001f + 0.001f;
+            //Console.WriteLine(a);
+
+            if( Math.Abs(a -  0.006f) <= 0.0001)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+
         }
     }
 }
