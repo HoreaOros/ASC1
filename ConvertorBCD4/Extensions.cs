4d9d0a3197cdeffa318f4907b1ff73f2dbaf55d4
Horea Oros <horea.oros@gmail.com>
2019-11-14 09:41:59 +0200

    add convert BCD project

---
+++ ConvertorBCD4/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorBCD4
+{
+    static class Extensions
+    {
+        public static string ConvertToBCD(this int n)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            int cifra;
+            while (n > 0)
+            {
+                cifra = n % 10;
+                n = n / 10;
+                stack.Push(cifra);
+            }
+
+            string[] bcd = {"0000", "0001", "0010", "0011", "0100",
+                "0101", "0110", "0111", "1000", "1001" };
+
+            StringBuilder sb = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                cifra = stack.Pop();
+                sb.Append(bcd[cifra]);
+                sb.Append(" ");
+            }
+            
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConvertorBCD4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertorBCD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 305; // 0011 0000 0101
+
+            Console.WriteLine(n.ConvertToBCD());
+            
+        }
+    }
+}
