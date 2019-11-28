9ba227ecc9485099b9478039580dee0dd62a5017
Horea Oros <horea.oros@gmail.com>
2019-11-28 09:13:11 +0200

    add bitpattern 3

---
+++ BitPattern3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitPattern3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 1u; // 00...0001
+            float f = (float)Math.Pow(2, 32);
+            byte[] b;
+            b = BitConverter.GetBytes(n);
+            Console.WriteLine(BitConverter.IsLittleEndian);
+            for (int i = b.Length - 1; i >= 0; i--)
+            {
+                // Console.Write($"{Convert.ToString(b[i], 2).PadLeft(8, '0')} ");
+                Console.Write($"{b[i].BitPattern()} ");
+            }
+        }
+    }
+}
---
+++ BitPattern3/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitPattern3
+{
+    static class Util
+    {
+        public static string BitPattern(this byte b)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 8; i++)
+            {
+                sb.Insert(0, ((b >> i) & 1) == 1? "1":"0");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
